ae7e5f3e81e6b94ce00ab4514f4db6831f473e76
Edson Batista <edson.batista@catho.com>
2021-03-09 23:54:58 -0300

    feat: get repositories from github API

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { RespositoryItem } from './RepositoryItem';
 
 import '../styles/repositories.scss';
@@ -9,16 +9,30 @@ const repository = {
   url: "https://github.com"
 }
 
+const REPO_URL = 'https://api.github.com/orgs/rocketseat/repos';
+
 export function RepositoryList() {
+  const [ repositories, setRepositories ] = useState([]);
+
+  useEffect(() => {
+    fetch(REPO_URL)
+      .then(response => response.json())
+      .then(setRepositories);
+  }, []);
+
   return(
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RespositoryItem repository={repository}/>
-        <RespositoryItem/>
-        <RespositoryItem/>
-        <RespositoryItem/>
+        { repositories.map(repository => {
+          const { name, description, html_url: url } = repository;
+
+          const repositoryProps = { name, description, url };
+
+          return <RespositoryItem key={repository.id} repository={repositoryProps}/>
+
+        }) }
       </ul>
     </section>
   )
