b0ff1887de0f71ba409831eedc98c27dad07b151
Edson Batista <edson.batista@catho.com>
2021-03-10 01:17:31 -0300

    chore: configure typrescript

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 module.exports = {
   mode: isDevelopment ? "development" : "production",
 
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
 
   devtool: isDevelopment ? "eval-source-map" : "source-map",
 
@@ -17,7 +17,7 @@ module.exports = {
   },
 
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
 
   devServer: {
@@ -35,7 +35,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.(j|t)sx$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
